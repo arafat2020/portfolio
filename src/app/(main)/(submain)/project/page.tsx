38f7c300a4dee59ef6ff5511e1b605cd265cbfc0
Arafat <arafatmannan2019@gmail.com>
2024-12-20 16:41:49 +0600

    shift

--- src/app/(main)/(submain)/project/page.tsx
+++ src/app/(main)/(submain)/project/page.tsx
@@ -7,34 +7,35 @@ import { SparkleHeading } from "../../_components/Sparklejeading";
 export default function MyProjectsPage() {
   const projects = [
     {
-      name: "Awesome Project 1",
-      description: "A description of the project showcasing the power of React and Next.js.",
+      name: "AnhaBd",
+      description: "A Next js website with headless CMS",
       tags: [
         { name: "React", color: "text-blue-500" },
         { name: "Next.js", color: "text-gray-500" },
+        { name: "TRPS", color: "text-sky-500" },
       ],
-      image: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      source_code_link: "https://github.com/yourusername/awesome-project-1",
+      image: "./project/anahbd.png",
+      source_code_link: "https://github.com/arafat2020/org",
     },
     {
-      name: "Awesome Project 2",
-      description: "A full-stack application built with React and Node.js.",
+      name: "Dev Motion",
+      description: "A Next js web app using convex as a backend",
       tags: [
         { name: "Full-stack", color: "text-green-500" },
-        { name: "Node.js", color: "text-yellow-500" },
+        { name: "Convex", color: "text-yellow-500" },
       ],
-      image: "https://images.unsplash.com/photo-1484215149000-017b0139b4f3?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      source_code_link: "https://github.com/yourusername/awesome-project-2",
+      image: "./project/dev-motion.png",
+      source_code_link: "https://github.com/arafat2020/dev_motion",
     },
     {
-      name: "Awesome Project 3",
-      description: "A creative project demonstrating Tailwind CSS with Next.js.",
+      name: "Hacker net",
+      description: "A SPA (Single Page App) using Spotify API",
       tags: [
-        { name: "Tailwind CSS", color: "text-indigo-500" },
-        { name: "Next.js", color: "text-gray-500" },
+        { name: "APi", color: "text-indigo-500" },
+        { name: "SPA", color: "text-cyan-500" },
       ],
-      image: "https://images.unsplash.com/photo-1502746870074-f9f6a79a9a4d?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      source_code_link: "https://github.com/yourusername/awesome-project-3",
+      image: "./project/hacker-tune.png",
+      source_code_link: "https://github.com/arafat2020/spotify_clone",
     },
   ];
 
